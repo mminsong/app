ff4081899424c0f1cbf5d969b0988505fec4c9a4
mminsong <pamten@naver.com>
2025-10-22 22:17:03 +0900

    todolist 취소선선

--- my-study-app/src/components/TodoList.tsx
+++ my-study-app/src/components/TodoList.tsx
@@ -1,33 +1,46 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
-const TodoList : React.FC=()=>{
-    // const [todos,setTodos]=useState<Todo[]>;
-    const [newTodo,setNewTodo]=useState('');
+interface Todo {
+    id: number;
+    text: string;
+    completed: boolean;
+}
 
-    const todoObject={
-        id: Number,
-        text: newTodo,
-        completed: false,
+const TodoList: React.FC = () => {
+    const [todos, setTodos] = useState<Todo[]>([]);
+    const [newTodo, setNewTodo] = useState("");
+
+    const insertTodo = () => {
+        if (newTodo.trim() === "")
+            return;
+        const newItem = {
+            id: Date.now(),
+            text: newTodo,
+            completed: false,
+        };
+        setTodos([...todos, newItem]);
+        setNewTodo("");
     }
-return(
-    <div>
-        <div>TodoList</div>
+
+
+    return (
         <div>
-            <div>Date</div>
-            <div className="list">
-                <div>
-                <input type="checkbox" name="todolist"/><div>할일1</div>
-                </div>
-                <div>
-                <input type="checkbox" name="todolist"/><div>할일2</div>
-                </div>
-                <div>
-                <input type="checkbox" name="todolist"/><div>할일3</div>
-                </div>
+            <div>
+                <div>TodoList</div>
+                <div>완성도</div>
+                <div>{todos.filter(todo => todo.completed).length}/{todos.length}</div>
+                <div></div>
             </div>
+            {todos.map((todo) => (
+                <div key={todo.id}>
+                    <input type="checkbox" checked={todo.completed} onChange={() => setTodos(todos.map((t) => t.id === todo.id ? { ...t, completed: !t.completed } : t))}></input>
+                    <span style={{ textDecoration: todo.completed ? "line-through" : "none" }}>{todo.text}</span>
+                </div>
+            ))}
+            <input type="text" placeholder='todolist를 입력하세요' value={newTodo} onChange={(e) => setNewTodo(e.target.value)}></input>
+            <button onClick={insertTodo}>추가</button>
         </div>
-    </div>
-)
+    )
 }
 
 export default TodoList;
